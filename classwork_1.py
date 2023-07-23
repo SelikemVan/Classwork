a7d81d367a5814bafaf07ebc9f286416b0a00000
SelikemVan <selikemvan@gmail.com>
2023-07-23 15:27:40 +0100

    First commit

--- classwork_1.py
+++ classwork_1.py
@@ -1,7 +1,7 @@
 # Write a Python function to find the maximum of three numbers.
 
 def maximum(a, b, c):
-    if a >= b and a >= c
+    if a >= b and a >= c:
         return a
     elif b >= a and b >= c:
         return b
